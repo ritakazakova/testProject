3af427efbf5f05cf4494d94e9747faa3edf6c185
Rita <mkazakova00@mail.ru>
2021-11-27 13:52:27 +0300

    Initial Commit

---
+++ testProject/testProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  testProjectApp.swift
+//  testProject
+//
+//  Created by Rita Kazakova on 11/27/21.
+//
+
+import SwiftUI
+
+@main
+struct testProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
